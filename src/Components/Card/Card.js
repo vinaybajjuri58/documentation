2254fd50fe0182b387b3175cab5febe2b0dc7c79
vinaybajjuri58 <vinaybajjuri58@gmail.com>
2021-04-03 14:48:39 +0530

    Style: Added horizonal card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -10,6 +10,7 @@ export const Card = () => {
     <div className="component-description">
       <h2 className="component-heading">All about Cards</h2>
       <div style={{ width: "100%" }}>
+        <p className="description">A normal card</p>
         <div className="card">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz-K6eEjSupJW5RaX7jUf_YPNkj5sHQ6JUTA&usqp=CAU"
@@ -39,7 +40,7 @@ export const Card = () => {
         </SyntaxHighlighter>
         <div>
           <p className="description">A Card for e-commerce</p>
-          <div className="card card-shopping">
+          <div className="card-shopping">
             <img
               src="https://images.unsplash.com/photo-1571455786673-9d9d6c194f90?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8YmxhY2slMjB0JTIwc2hpcnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
               alt=""
@@ -54,7 +55,7 @@ export const Card = () => {
         <div style={{ width: "auto" }}>
           <SyntaxHighlighter language="javascript" style={a11yDark}>
             {`Code Snippet
-<div class="card card-shopping">
+<div class="card-shopping">
   <img src="image-url" alt="" class="card-img" />
   <h4 class="card-brand">Brand Name</h4>
   <p class="card-desc">A Black T-Shirt</p>
@@ -63,6 +64,44 @@ export const Card = () => {
 </div> `}
           </SyntaxHighlighter>
         </div>
+      </div>
+      <p className="description">A Horizontal card</p>
+      <div className="card-horizontal">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz-K6eEjSupJW5RaX7jUf_YPNkj5sHQ6JUTA&usqp=CAU"
+          alt=""
+          className="card-img"
+        />
+        <div className="card-content">
+          <h4 className="card-title">Card Title</h4>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis et
+            architecto dicta. Placeat, aspernatur, commodi impedit dicta id,
+            explicabo porro cumque ullam dolore saepe fuga? Ullam porro
+            voluptatum suscipit dolor!
+          </p>
+        </div>
+      </div>
+      <div style={{ width: "auto" }}>
+        <SyntaxHighlighter language="javascript" style={a11yDark}>
+          {`Code Snippet
+<div className="card-horizontal">
+  <img
+  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz-K6eEjSupJW5RaX7jUf_YPNkj5sHQ6JUTA&usqp=CAU"
+  alt=""
+  className="card-img"
+  />
+  <div className="card-content">
+    <h4 className="card-title">Card Title</h4>
+    <p className="card-text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis et
+      architecto dicta. Placeat, aspernatur, commodi impedit dicta id,
+      explicabo porro cumque ullam dolore saepe fuga? Ullam porro
+      voluptatum suscipit dolor!
+    </p>
+  </div>
+</div>`}
+        </SyntaxHighlighter>
       </div>
     </div>
   );
